e0cdc8024ae712a162e7ab7dc729aba46bec95d1
ErgiIsmahili <ergi.ismahili@gmail.com>
2023-04-02 18:28:58 -0400

    small changes

--- auth_user/tests.py
+++ auth_user/tests.py
@@ -1,10 +1,12 @@
 from django.test import TestCase
 from auth_user.views import *
 from auth_user.models import CustomAccount
+import json
 
 class TestBasic(TestCase):
-    
-    def test_create_account_view(self):
+
+    def test_create_account(self):
+        # Test Correct Create Account
         response = self.client.post(
             path='http://127.0.0.1:8080/account/create',
             data={
@@ -19,21 +21,7 @@ class TestBasic(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue("user_created" in response.json())
 
-    def test_create_account_existing(self):
-        response = self.client.post(
-            path='http://127.0.0.1:8080/account/create',
-            data={
-                "email_address": "test2@test.com",
-                "password": "testpassword",
-                "first_name": "Test",
-                "last_name": "User",
-                "company": "Duck Creek",
-                "pay_rate": 10,
-            },
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue("user_created" in response.json())
-
+        # Test Create Existing Account
         response = self.client.post(
             path='http://127.0.0.1:8080/account/create',
             data={
@@ -46,4 +34,40 @@ class TestBasic(TestCase):
             },
         )
         self.assertEqual(response.status_code, 500)
+
+        # Test Missing Data Feilds
+        response = self.client.post(
+            path='http://127.0.0.1:8080/account/create',
+            data={
+                "email_address": "test2@test.com",
+                "password": "test",
+                "first_name": "User",
+                "last_name": "Name",
+            },
+        )
+        self.assertEqual(response.status_code, 500)
+
  
+    def test_login(self):
+        response = self.client.post(
+            path='http://127.0.0.1:8080/account/create',
+            data={
+                "email_address": "test2@test.com",
+                "password": "testpassword",
+                "first_name": "Test",
+                "last_name": "User",
+                "company": "Duck Creek",
+                "pay_rate": 10,
+            },
+        )
+
+        response = self.client.post(
+            path='http://127.0.0.1:8080/account/login',
+            data={
+                "email_address": "test2@test.com",
+                "password": "testpassword",
+            },
+        )
+
+        self.assertTrue(json.loads(response.content)['login_success'])
+
