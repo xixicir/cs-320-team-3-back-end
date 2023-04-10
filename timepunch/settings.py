f76e38751954ce2f6d2ec33bdf2530b46740f65a
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2023-04-10 14:57:32 -0400

    Added localhost to origins

--- timepunch/settings.py
+++ timepunch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "oif83u989-ltbj0hg+bby5vo#=l&=c824mjc=q%it25r(gely%waec-vp@(a"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost"]
 
 # Application definition
 
