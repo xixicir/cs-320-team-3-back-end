b37c4dbd87aa1ea6a2de5fd56ff2344489754baa
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2023-04-10 15:08:48 -0400

    Added cors

--- timepunch/settings.py
+++ timepunch/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     "time_log",
     "django_nose",
     "drf_yasg",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -53,6 +55,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
 ]
 
 ROOT_URLCONF = "timepunch.urls"
